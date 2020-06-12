365f06b455a2a4dd8e575ab2ef2eaad9b58a562b
sergey_dyachkov <dyachkov67@gmail.com>
2020-06-13 00:45:30 +0300

    What's wrong

---
+++ Problems/Vowels and consonants/task.py
@@ -0,0 +1,18 @@
+word = input()
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V',
+            'W', 'X', 'Y', 'Z', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j',
+            'k',
+            'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
+consonants = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z',
+              'b',
+              'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+for char in word:
+    if char in alphabet:
+        if char in consonants:
+            print('consonant ')
+        elif char in vowels:
+            print('vowel')
+    else:
+        break
